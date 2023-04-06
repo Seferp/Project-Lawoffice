e072719287f10a80b865ec34109dbdd571b244fe
P.Seferyniak <p.seferyniak@gmail.com>
2023-04-06 21:53:36 +0200

    Created view on the main page

--- lawoffice/my_site/urls.py
+++ lawoffice/my_site/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.home, name='home'),
+    path('o-mnie', views.about_me_view, name='about_me'),
+    path('kontakt', views.contact_view, name='contact'),
 ]
--- lawoffice/my_site/views.py
+++ lawoffice/my_site/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def home(request):
+    return render(request, 'my_site/home.html')
 
+
+def about_me_view(request):
+    return render(request, 'my_site/about-me.html')
+
+def contact_view(request):
+    return render(request, 'my_site/contact.html')

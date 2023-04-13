6912a7fa5d8f4f689569dc5be10fcd8953da4990
P.Seferyniak <p.seferyniak@gmail.com>
2023-04-13 22:33:57 +0200

    Created cart

--- lawoffice/my_site/forms.py
+++ lawoffice/my_site/forms.py
@@ -3,6 +3,11 @@ from django import forms
 from .models import Contact
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(label= 'Imię', max_length=100)
+    last_name = forms.CharField(label= 'Nazwisko', max_length=100)
+    email = forms.EmailField(label= 'Email')
+    subject = forms.CharField(label= 'Temat', max_length=200)
+    message = forms.CharField(label= 'Wiadomość', widget=forms.Textarea)
     class Meta:
         model = Contact
         fields = ['first_name', 'last_name', 'email', 'subject', 'message']

bdce8ab7a75d6edde93d0bfa9cd59691e3de0204
P.Seferyniak <p.seferyniak@gmail.com>
2023-07-02 22:56:19 +0200

    Merge remote-tracking branch 'origin/prepare_for_production' into prepare_for_production
    # Conflicts:
    #	lawoffice/requirements.txt
    #	lawoffice/static/base.css

--- lawoffice/blog/views.py
+++ lawoffice/blog/views.py
@@ -2,17 +2,19 @@ from django.shortcuts import render, HttpResponseRedirect, reverse
 from django.views.generic import ListView, View
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseNotFound
+from django.utils.html import format_html
 
 from .models import Post
 from .forms import CommentForm
 
+import re
 # Create your views here.
 
 
 class BlogHomePage(ListView):
     model = Post
     template_name = 'blog/blog-main-page.html'
-    ordering = ['-date']
+    ordering = ['-id']
     context_object_name = 'posts'
 
 
@@ -20,6 +22,7 @@ class SinglePost(View):
 
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
+        post.content = self.add_hard_space(post.content)
 
         context = {
             "post": post,
@@ -56,3 +59,15 @@ class SinglePost(View):
         context['post_tags'] = self.objects.tag.all()
         context['comment_form'] = CommentForm()
         return context
+
+
+
+    def add_hard_space(self, text):
+        regex = r"\b([a-zA-Z]{1,3})\s?\b"
+        new_text = re.sub(regex, r'\1&nbsp;', text)
+        return new_text
+
+    # def post_detail(self, request, post_id):
+    #     post = Post.objects.get(pk=post_id)
+    #     post.content = self.add_hard_space(post.content)
+    #     return render(request, 'post_detail.html', {'post': post})
